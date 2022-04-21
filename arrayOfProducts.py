9ea33af811428e1d855aef4f03fdf7879c84f5ce
Nischal <nischal.timalsina@gmail.com>
2022-04-20 20:55:10 -0400

    arrayOfProductsSolution2

--- arrayOfProducts.py
+++ arrayOfProducts.py
@@ -4,20 +4,20 @@
 #**************************************###############################*************************************
 #ALGOEXPERT-MEDIUM-Q-Array Of Products-Solution 1:
 
-def arrayOfProducts(array):
-    products = [1 for _ in range(len(array))]
+# def arrayOfProducts(array):
+#     products = [1 for _ in range(len(array))]
 
-    for i in range(len(array)):
-        runningProduct = 1
-        for j in range(len(array)):
-            if i!=j:
-                runningProduct *= array[j]
-        products[i] = runningProduct
+#     for i in range(len(array)):
+#         runningProduct = 1
+#         for j in range(len(array)):
+#             if i!=j:
+#                 runningProduct *= array[j]
+#         products[i] = runningProduct
 
-    return products
+#     return products
 
 
-print(arrayOfProducts([5,1,4,2]))
+# print(arrayOfProducts([5,1,4,2]))
 
 
 
@@ -28,13 +28,23 @@ print(arrayOfProducts([5,1,4,2]))
 
 def arrayOfProducts(array):
     products = [1 for _ in range(len(array))]
+    leftProducts = [1 for _ in range(len(array))]
+    rightProducts = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
 
     for i in range(len(array)):
-        runningProduct = 1
-        for j in range(len(array)):
-            if i!=j:
-                runningProduct *= array[j]
-        products[i] = runningProduct
+       leftProducts[i] = leftRunningProduct
+       leftRunningProduct *= array[i]
+    
+    rightRunningProduct = 1
+
+    for i in reversed(range(len(array))):
+        rightProducts[i] = rightRunningProduct
+        rightRunningProduct *= array[i]
+
+    for i in range(len(array)):
+        products[i] = leftProducts[i]* rightProducts[i]
 
     return products
 
